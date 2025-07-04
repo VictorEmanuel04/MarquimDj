6866b58679d4cb8f8a98dec4891e3a6d229c0207
Your Name <you@example.com>
2025-07-04 11:04:38 -0300

    Adicionada visualização inicial do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Land from './components/Land';
 
 function App() {
 
 
   return (
     <>
+    <Land/>
     </>
   )
 }
--- src/components/Land.jsx
+++ src/components/Land.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import '../styles/Land.css'
 
 const Land = () => {
   return (
-    <div>Land</div>
+     <div className="app">
+      <header className="header">
+        <div className="logo">🎵 MDJ</div>
+        <nav className="nav">
+          <a href="#">Início</a>
+          <a href="#">Sobre</a>
+          <a href="#">Apresentações</a>
+          <a href="#">Formulário</a>
+        </nav>
+      </header>
+
+      <section className="center-picture">
+        <h1>Início</h1>
+        <img
+          src="https://images.unsplash.com/photo-1518976024611-488d8b6e1c9c?auto=format&fit=crop&w=800&q=80"
+          alt="Festa / DJ"
+        />
+        <div className="title">MARQUIM DJ</div>
+        <div className="slogan">"Conectando pessoas através da música!"</div>
+      </section>
+      
+      <div className='about'>
+        <section className='about-picture'>
+            <div className='about-title'>
+                <h3>Sobre</h3>
+            <img 
+                src="" 
+                alt="Dj" 
+            />
+                <div className='about-text'>Sou DJ porque falar com música é bem mais legal. Me inspiro no
+                    groove do funk, nas batidas certeiras do house e na vibe futurista
+                    do techno. Misturo tudo pra fazer a pista dançar sem pensar em
+                    mais nada. Som na caixa e bora curtir!
+                </div>
+            </div>
+        </section>
+      </div>
+    </div>
+
   )
 }
 
